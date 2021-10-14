d54c2dfdf78af93710fac50ff88f6631de715592
Zack Lyon <zack.j.lyon@gmail.com>
2021-10-14 11:44:31 -0700

    Frontend with search page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { fetchBooks } from './request-utils.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  state = {
+    title: '',
+    booksList: []
+  }
+
+  handleSubmit = async(e) => {
+    e.preventDefault();
+
+    const books = await fetchBooks(this.state.title);
+    await this.setState({booksList: books});
+    console.log("title ", this.state.title);
+    console.log("bookslist ", this.state.booksList);
+  }
+
+  render() {
+    const { booksList } = this.state;
+    console.log(booksList);
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Title :
+            <input value={this.state.title} onChange={(e) => this.setState({title: e.target.value})}></input>
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+
+        <div>
+          {
+            booksList.map(({ author, titleweb, isbn, flapcopy, priceusa }) => <div key={isbn}>
+              <h1>{author}</h1>
+              <h2>{titleweb}</h2>
+              <div>{flapcopy}</div>
+              <div> Price: ${priceusa} </div>
+            </div>)
+          }
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
